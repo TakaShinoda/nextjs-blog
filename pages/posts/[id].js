0aa9e0eda31ba330c61a998a69945ae154dc8c10
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-26 11:38:37 +0900

    Fix: 日付のフォーマットの変更

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import { Layout } from '../../components/Layout'
+import { Date } from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticPaths() {
     const paths = getAllPostIds()
@@ -25,13 +27,13 @@ export default function Post({ postData }) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={utilStyles.headingX}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
